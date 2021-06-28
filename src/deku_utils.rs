b999c231fc77fe8d59dbae8ef5c8778759035aa4
anirudhb <anirudhb@users.noreply.github.com>
2021-06-27 20:17:12 -0700

    feat: add initial header parser

---
+++ src/deku_utils.rs
@@ -0,0 +1,23 @@
+use deku::bitvec::{BitSlice, BitVec, Msb0};
+use deku::prelude::*;
+use deku::DekuError;
+
+pub(crate) fn read_string(
+    rest: &BitSlice<Msb0, u8>,
+) -> Result<(&BitSlice<Msb0, u8>, String), DekuError> {
+    use deku::ctx::{Endian, Limit};
+
+    let (rest, len) = u16::read(rest, Endian::Big)?;
+    let (rest, bytes) = Vec::read(rest, Limit::new_count(len as _))?;
+    Ok((
+        rest,
+        String::from_utf8(bytes).map_err(|e| DekuError::Parse(e.to_string()))?,
+    ))
+}
+
+pub(crate) fn write_string(output: &mut BitVec<Msb0, u8>, s: &str) -> Result<(), DekuError> {
+    (s.len() as u16).write(output, ())?;
+    s.as_bytes().write(output, ())?;
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,40 @@
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+mod deku_utils;
+
+use deku::prelude::*;
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+pub struct EmojipackHeader {
+    version: EmojipackVersion,
+    signature: [u8; blake3::OUT_LEN],
+    #[deku(
+        reader = "deku_utils::read_string(deku::rest)",
+        writer = "deku_utils::write_string(deku::output, &self.name)"
+    )]
+    name: String,
+}
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(type = "u8")]
+pub enum EmojipackVersion {
+    One = 0, // confusing, I know
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use hexlit::hex;
 
     #[test]
-    fn it_works() {
-        assert_eq!(add(2, 2), 2 + 2);
+    fn test_simple_header() {
+        let data: &[u8] =
+            &hex!("00264bcafcc26080c228c47ac79a017f1c9427886607fcf744c0c284486098bdab000474657374");
+        let ((_, left), header) = EmojipackHeader::from_bytes((data, 0)).unwrap();
+        assert_eq!(left, 0);
+        assert_eq!(header.version, EmojipackVersion::One);
+        assert_eq!(
+            header.signature,
+            hex!("264bcafcc26080c228c47ac79a017f1c9427886607fcf744c0c284486098bdab") // "The great amazing emojipack data"
+        );
+        assert_eq!(header.name, "test");
     }
 }
